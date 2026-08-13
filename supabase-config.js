d792d09cb99b2abdade0e420fe7602c46a198eeb
doananhtuant02 <doananhtuant02@gmail.com>
2026-08-13 09:45:24 +0700

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.AVP_SUPABASE_CONFIG = {
   url: "https://itnnbyhlpfredaqyhlpr.supabase.co",
-  publishableKey: "sb_publishable_eswla4x2YD3mkRO0o3l_3A_E9loQwk4"
+  publishableKey: "sb_publishable_MiK_gkzfQUwPn3_-TYZESg_FxuWYTV5"
 };
