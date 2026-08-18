bae5a9599ac5e51884a8b2872fdc776dfc094cb1
doananhtuant02 <doananhtuant02@gmail.com>
2026-08-18 19:10:07 +0700

    New video

--- practice-files-manifest.js
+++ practice-files-manifest.js
@@ -8,11 +8,12 @@
  */
 const availablePracticeFiles = [
   "video-01-viet-nam-hidden-space.xlsx"
+  "Tip2_Copy_Khong_Lay_Dong_An_AltSemicolon.xlsx"
 ];
 
 const videoTikTokLinks = {
   "video-01": "https://www.tiktok.com/@anhvanphongmaster/video/7675230261172391186?is_from_webapp=1&sender_device=pc&web_id=7670058834664211984",
-  "video-02": "",
+  "video-02": "https://www.tiktok.com/@anhvanphongmaster/video/7675341530072304903?is_from_webapp=1&sender_device=pc&web_id=7670058834664211984",
   "video-03": "",
   "video-04": "",
   "video-05": "",
