6a80998b2f3d5ec025db2be1bc8c7cefbf5bfc66
doananhtuant02 <doananhtuant02@gmail.com>
2026-08-19 20:37:31 +0700

    Update practice-files-manifest.js

--- practice-files-manifest.js
+++ practice-files-manifest.js
@@ -4,7 +4,7 @@ const availablePracticeFiles = [
 const practiceFileAliases = {};
 const videoTikTokLinks = {
   "video-01": "",
-  "video-02": "",
-  "video-03": "",
-  "video-04": ""
+  "video-02": "https://www.tiktok.com/@anhvanphongmaster/video/7674264336130526471?is_from_webapp=1&sender_device=pc&web_id=7670058834664211984",
+  "video-03": "https://www.tiktok.com/@anhvanphongmaster/video/7674481865058946312?is_from_webapp=1&sender_device=pc&web_id=7670058834664211984",
+  "video-04": "https://www.tiktok.com/@anhvanphongmaster/video/7674650382714391815?is_from_webapp=1&sender_device=pc&web_id=7670058834664211984"
 };
