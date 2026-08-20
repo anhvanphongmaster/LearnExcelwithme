94d17892a24b5e4f933f76a96877ea096b5d976f
doananhtuant02 <doananhtuant02@gmail.com>
2026-08-21 00:10:24 +0700

    Update practice files and add aliases
    Added multiple practice files and their aliases to the manifest.

--- practice-files-manifest.js
+++ practice-files-manifest.js
@@ -1,10 +1,49 @@
 const availablePracticeFiles = [
   "PowerQuery_DEMO.xlsx",
-  "PowerQuery-11-Files.zip"
-]
+  "PowerQuery-11-Files.zip",
+  "01_PowerQuery_DEMO.xlsx",
+  "02_PowerQuery-11-Files.zip",
+  "05_center_across.xlsx",
+  "06_so_viet.xlsx",
+  "07_an_so_0.xlsx",
+  "08_dan_dong_loc.xlsx",
+  "V08_center_across_thuc_hanh.xlsx",
+  "V10_so_viet_thuc_hanh.xlsx",
+  "V11_an_so_0_thuc_hanh.xlsx",
+  "B01_dan_vao_dong_loc_thuc_hanh.xlsx"
+];
+
+const practiceFileAliases = {
+  "01_PowerQuery_DEMO.xlsx": "PowerQuery_DEMO.xlsx",
+  "02_PowerQuery-11-Files.zip": "PowerQuery-11-Files.zip",
+  "05_center_across.xlsx": "V08_center_across_thuc_hanh.xlsx",
+  "06_so_viet.xlsx": "V10_so_viet_thuc_hanh.xlsx",
+  "07_an_so_0.xlsx": "V11_an_so_0_thuc_hanh.xlsx",
+  "08_dan_dong_loc.xlsx": "B01_dan_vao_dong_loc_thuc_hanh.xlsx"
+};
+
 const videoTikTokLinks = {
   "video-01": "https://vt.tiktok.com/ZSVDNoRHE/",
   "video-02": "https://vt.tiktok.com/ZSVAbxkbF/",
   "video-03": "https://vt.tiktok.com/ZSVAbk8rn/",
-  "video-04": "https://vt.tiktok.com/ZSVAqt4DM/"
+  "video-04": "https://vt.tiktok.com/ZSVAqt4DM/",
+  "v02-center-across": "",
+  "v04-so-viet": "",
+  "v05-an-so-0": "",
+  "b01-dan-dong-loc": "",
+  "c2-sum-text": "",
+  "c3-ngay": "",
+  "v01-char160": "",
+  "v03-alt-visible": "",
+  "v06-dropdown-2tang": "",
+  "v07-xlookup": "",
+  "v08-unpivot": "",
+  "v09-from-folder": "",
+  "v10-fuzzy": "",
+  "c1-pq-10sheet": "",
+  "v11-skip-blanks": "",
+  "v12-textbefore": "",
+  "v13-filldown": "",
+  "v14-distinct": "",
+  "v15-aggregate": ""
 };
